9d9a0c5f8f979a42f467a93d5060bf5ab8a09e7c
Lorenzo Calamandrei <lorenzo.calamandrei@thinkopen.it>
2017-10-31 11:08:23 +0100

    Shipping Method
    The module of the shipping time can't be activated if the multishipping is activated. (Work fine)

---
+++ project/app/code/local/NaN/Shipping/Model/adminhtml/EnabledControl.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class NaN_Shipping_Model_adminhtml_enabledControl
+ */
+
+/**
+ * Class NaN_Shipping_Model_adminhtml_enabledControl
+ * @author  NaN Team
+ * @version 0.1.0
+ * @package Shipping
+ */
+class NaN_Shipping_Model_adminhtml_enabledControl extends Mage_Core_Model_Config_Data
+{
+    public function _afterSave()
+    {
+        if (!Mage::getStoreConfig('shipping/option/checkout_multiple')) {
+            return parent::_afterSave();
+        }
+        Mage::getModel('core/session')->addError(Mage::helper('nan_shipping')->__('The module can\'t be activated if the multishipping is activated. Sorry Man.'));
+        Mage::getModel('core/config')->saveConfig('carriers/nan_shipping/active', '0');
+        return parent::_afterSave();
+    }
+}
+
